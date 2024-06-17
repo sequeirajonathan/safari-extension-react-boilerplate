f93524aaad369c011d32e4d8823acf00578ba530
sequeirajonathan <sequeira.s.jonathan@gmail.com>
2024-06-17 12:28:15 -0500

    updated webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,42 +1,42 @@
-const path = require("path");
+const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const ejs = require('ejs');
 const { version } = require('./package.json');
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
+  context: __dirname,  // Set context to the root directory
   entry: {
-    'popup/popup': "./popup/index.tsx",
-    content: "./content.ts",
-    background: "./background.ts",
+    popup: './src/popup/index.tsx',
+    content: './src/content.ts',
+    background: './src/background.ts',
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].js"
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader"
+        use: 'ts-loader',
       },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader" // Creates style nodes from JS strings
+            loader: 'style-loader', // Creates style nodes from JS strings
           },
           {
-            loader: "css-loader" // Translates CSS into CommonJS
+            loader: 'css-loader', // Translates CSS into CommonJS
           },
           {
-            loader: "sass-loader" // Compiles Sass to CSS
-          }
-        ]
-      }
-    ]
+            loader: 'sass-loader', // Compiles Sass to CSS
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new CopyPlugin({
@@ -47,32 +47,32 @@ module.exports = {
           globOptions: {
             dot: true,
             gitignore: true,
-            ignore: ['**/manifest.json', '**/.DS_Store'],
+            ignore: ['**/.DS_Store'],
           },
           noErrorOnMissing: true,
         },
-        // Append variables to popup.html
-        { 
-          from: path.resolve(__dirname, 'src/popup/popup.html'), 
-          to: path.resolve(__dirname, 'dist/popup/popup.html'), 
-          transform: transformHtml 
+        // Place popup.html in the root of dist
+        {
+          from: path.resolve(__dirname, 'src/popup/popup.html'),
+          to: path.resolve(__dirname, 'dist/popup.html'),
+          transform: transformHtml,
         },
         // Update manifest version from package.json
         {
           from: path.resolve(__dirname, 'public/manifest.json'),
           to: path.resolve(__dirname, 'dist/manifest.json'),
-          transform: (content) => {
+          transform: content => {
             const jsonContent = JSON.parse(content);
             jsonContent.version = version;
             return JSON.stringify(jsonContent, null, 2);
           },
         },
-      ]
+      ],
     }),
   ],
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  }
+    extensions: ['.ts', '.tsx', '.js'],
+  },
 };
 
 function transformHtml(content) {
